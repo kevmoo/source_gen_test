6ac2fb8abdba63641e3a24f987bafe01f14b3ed2
Kevin Moore <kevmoo@users.noreply.github.com>
2019-03-04 12:19:51 -0800

    feature: initLibrary (#2)

---
+++ test/src/test_library.dart
@@ -0,0 +1,80 @@
+import 'package:source_gen_test/annotations.dart';
+
+import 'test_annotation.dart';
+
+part 'test_part.dart';
+
+@ShouldGenerate(
+  r'''
+const TestClass1NameLength = 10;
+
+const TestClass1NameLowerCase = testclass1;
+''',
+  configurations: ['default', 'no-prefix-required'],
+)
+@ShouldThrow(
+  'Uh...',
+  configurations: ['vague'],
+  element: false,
+)
+@TestAnnotation()
+class TestClass1 {}
+
+@ShouldThrow(
+  'All classes must start with `TestClass`.',
+  todo: 'Rename the type or remove the `TestAnnotation` from class.',
+  configurations: ['default'],
+  expectedLogItems: ['This member might be not good.'],
+)
+@ShouldGenerate(
+  r'''
+const BadTestClassNameLength = 12;
+
+const BadTestClassNameLowerCase = badtestclass;
+''',
+  configurations: ['no-prefix-required'],
+  expectedLogItems: ['This member might be not good.'],
+)
+@ShouldThrow(
+  'Uh...',
+  configurations: ['vague'],
+  element: false,
+)
+@TestAnnotation()
+class BadTestClass {}
+
+@ShouldThrow(
+  'Cannot generate for classes with members that include `unsupported` in '
+      'their name.',
+  element: 'unsupportedFunc',
+  configurations: ['default'],
+  expectedLogItems: ['This member might be not good.'],
+)
+@TestAnnotation()
+class TestClassWithBadMember {
+  void unsupportedFunc() {}
+}
+
+@ShouldThrow(
+  'Only supports annotated classes.',
+  todo: 'Remove `TestAnnotation` from the associated element.',
+)
+@ShouldThrow(
+  'Uh...',
+  configurations: ['vague'],
+  element: false,
+)
+@TestAnnotation()
+int badTestFunc() => 42;
+
+// TODO: investigate annotated fields
+/*
+BUGGY!
+
+@ShouldThrow(
+  'All classes must start with `TestClass`.',
+  todo: 'Rename the type or remove the `TestAnnotation` from class.',
+)
+@TestAnnotation()
+final badTestField = 42;
+*/
---
+++ test/src/test_part.dart
@@ -0,0 +1,14 @@
+part of 'test_library.dart';
+
+@ShouldGenerate(r'''
+const TestClass2NameLength = 10;
+
+const TestClass2NameLowerCase = testclass2;
+''')
+@ShouldThrow(
+  'Uh...',
+  configurations: ['vague'],
+  element: false,
+)
+@TestAnnotation()
+class TestClass2 {}
