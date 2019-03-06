dfc9928e22af6806848e816cd4ed02c524cc625b
Kevin Moore <kevmoo@google.com>
2019-03-06 11:27:26 -0800

    working example with tests

---
+++ example/example.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+import 'package:source_gen_test/src/build_log_tracking.dart';
+import 'package:source_gen_test/src/init_library_reader.dart';
+import 'package:source_gen_test/src/test_annotated_classes.dart';
+
+import 'example_generator.dart';
+
+Future<void> main() async {
+  final reader = await initializeLibraryReaderForDirectory(
+      'example/src', 'example_test_src.dart');
+
+  initializeBuildLogTracking();
+  testAnnotatedElements(
+    reader,
+    const ExampleGenerator(),
+    additionalGenerators: const {
+      'no-prefix-required': ExampleGenerator(requireTestClassPrefix: false)
+    },
+  );
+}
---
+++ example/src/example_test_src.dart
@@ -0,0 +1,58 @@
+import 'package:source_gen_test/annotations.dart';
+
+import 'example_annotation.dart';
+
+@ShouldGenerate(
+  r'''
+const TestClass1NameLength = 10;
+
+const TestClass1NameLowerCase = testclass1;
+''',
+  configurations: ['default', 'no-prefix-required'],
+)
+@ExampleAnnotation()
+class TestClass1 {}
+
+@ShouldGenerate(
+  r'''
+const BadTestClassNameLength = 12;
+
+const BadTestClassNameLowerCase = badtestclass;
+''',
+  configurations: ['no-prefix-required'],
+  expectedLogItems: ['This member might be not good.'],
+)
+@ShouldThrow(
+  'All classes must start with `TestClass`.',
+  todo: 'Rename the type or remove the `TestAnnotation` from class.',
+  configurations: ['default'],
+  expectedLogItems: ['This member might be not good.'],
+)
+@ExampleAnnotation()
+class BadTestClass {}
+
+@ShouldThrow(
+  'Cannot generate for classes with members that include `unsupported` in '
+      'their name.',
+  element: 'unsupportedFunc',
+  configurations: ['default'],
+  expectedLogItems: ['This member might be not good.'],
+)
+@ExampleAnnotation()
+class TestClassWithBadMember {
+  void unsupportedFunc() {}
+}
+
+@ShouldThrow(
+  'Only supports annotated classes.',
+  todo: 'Remove `TestAnnotation` from the associated element.',
+)
+@ExampleAnnotation()
+int badTestFunc() => 42;
+
+@ShouldThrow(
+  'Only supports annotated classes.',
+  todo: 'Remove `TestAnnotation` from the associated element.',
+)
+@ExampleAnnotation()
+final badTestField = 42;
