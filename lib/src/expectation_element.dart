32048649c1499d35d3a08295ac58d2c2361a773f
Kevin Moore <kevmoo@users.noreply.github.com>
2021-05-22 20:32:33 -0700

    Remove unneeded imports (#21)

--- lib/src/expectation_element.dart
+++ lib/src/expectation_element.dart
@@ -2,7 +2,6 @@ import 'package:analyzer/dart/constant/value.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:source_gen/source_gen.dart';
 
-import '../source_gen_test.dart';
 import 'annotations.dart';
 
 List<_ExpectationElement> genAnnotatedElements(
