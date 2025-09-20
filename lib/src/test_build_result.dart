119b2dcfef03fe7d76da12c14a4ff8f15f7ea70c
d-markey <d-markey@users.noreply.github.com>
2025-09-21 00:25:53 +0200

    Expose asset IDs in `TestBuildResult`

--- lib/src/test_build_result.dart
+++ lib/src/test_build_result.dart
@@ -1,12 +1,13 @@
 import 'package:build/build.dart';
 
 class TestBuildResult {
-  TestBuildResult._();
+  TestBuildResult._(this.inputId);
 
+  final AssetId inputId;
   final _outputs = <AssetId, String>{};
 
   static Future<TestBuildResult> from(BuildStep step) async {
-    final results = TestBuildResult._();
+    final results = TestBuildResult._(step.inputId);
     for (var id in step.allowedOutputs) {
       try {
         final code = await step.readAsString(id);
@@ -17,6 +18,8 @@ class TestBuildResult {
     }
     return results;
   }
+
+  Iterable<AssetId> get outputs => _outputs.keys;
 
   String? getGeneratedContents(AssetId outputId) => _outputs[outputId];
 }
